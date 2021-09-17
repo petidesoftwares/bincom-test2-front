f80dd1ee1c6a7a1864fbf8d88934b925d3b70099
petidesoftwares <petideenterprise@gmail.com>
2021-09-17 13:49:21 +0100

    Update question2Controller.js

--- javascripts/question2Controller.js
+++ javascripts/question2Controller.js
@@ -21,8 +21,8 @@ function getResult(){
     $("#lga-result-body").html("");
     $.get(appUrl+'api/lga/result/sum/'+lgaID, function (data, status){
         console.log(data);
-        for (let i=0; i<data.lga_result.length; i++){
-            $("#lga-result-body").append("<tr><td>"+data.lga_result[i].party_abbreviation+"</td><td>"+data.lga_result[i].party_score+"</td></tr>");
-        }
+        // for (let i=0; i<data.lga_result.length; i++){
+        //     $("#lga-result-body").append("<tr><td>"+data.lga_result[i].party_abbreviation+"</td><td>"+data.lga_result[i].party_score+"</td></tr>");
+        // }
     })
 }
